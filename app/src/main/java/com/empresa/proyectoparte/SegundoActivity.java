3b892dcd834251a7db8bcddc813e2c81dc4e522e
Stevens <stevens.sifuentes@unmsm.edu.pe>
2022-01-21 17:27:15 -0500

    Registrar usuario con creación de historial clínica

---
+++ app/src/main/java/com/empresa/proyectoparte/SegundoActivity.java
@@ -0,0 +1,14 @@
+package com.empresa.proyectoparte;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SegundoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
