fee7a8ed9a76849c882192ce8357555786e9e14d
Stevens <stevens.sifuentes@unmsm.edu.pe>
2022-01-21 17:07:26 -0500

    Interfaz Logueo de usuario

---
+++ app/src/main/java/android/example/app/CrearHistorialMedico.java
@@ -0,0 +1,14 @@
+package android.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearHistorialMedico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_historial_medico);
+    }
+}
